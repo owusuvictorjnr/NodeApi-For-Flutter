678f30b5bd249863bd85ce2c8360d6954aac64d5
owusuvictorjnr <owusuvictor80@gmail.com>
2023-07-23 01:51:22 -0400

    Connect the db

--- config/db.js
+++ config/db.js
@@ -1,33 +1,12 @@
 const mongoose = require('mongoose')
 
-const connection = {}
+const connection = mongoose
+  .createConnection('mongodb://127.0.0.1:27017')
+  .on('open', () => {
+    console.log('MongoDb is connected')
+  })
+  .on('error', () => {
+    console.log('MongoDb connection error')
+  })
 
-async function connect() {
-  if (connection.isConnected) {
-    console.log('Already connected')
-    return
-  }
-
-  if (mongoose.connections.length > 0) {
-    connection.isConnected = mongoose.connections[0].readyState
-    if (connection.isConnected === 1) {
-      console.log('Use previous connection')
-      return
-    }
-    await mongoose.disconnect()
-  }
-}
-
-// disconnection
-async function disconnect() {
-  if (connection.isConnected) {
-    if (process.env.NODE_ENV === 'production') {
-      await mongoose.disconnect()
-      connection.isConnected = false
-    } else {
-      console.log('Not connected')
-    }
-  }
-}
-
-module.exports = connect
+module.exports = connection
